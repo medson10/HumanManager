41a1a3c9912bdcdb54cbef68d96517ebaaa878b8
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-06-07 22:43:39 -0300

    Add structure app and database config

---
+++ app/models/Funcionario.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+module.exports = function() {
+  let schema = mongoose.Schema({
+    nome: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    cargo: {
+      type: String,
+      required: true
+    },
+    genero: {
+      type: String,
+      required: true
+    },
+    telefone: {
+      type: String
+    },
+    setor: {
+      type: String,
+      required: true
+    }
+
+  });
+
+  return mongoose.model('Funcionario', schema);
+};
