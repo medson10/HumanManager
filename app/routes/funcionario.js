57be55210f2932480adc464fa8058a76440e4764
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-06-11 13:32:14 -0300

    Add funcionario routes

---
+++ app/routes/funcionario.js
@@ -0,0 +1,11 @@
+module.exports = function(app) {
+  var controller = app.controllers.funcionario;
+
+  app.route('/funcionarios')
+    .get(controller.listaFuncionarios)
+    .post(controller.salvaFuncionario);
+
+  app.route('/funcionarios/:id')
+    .get(controller.obtemFuncionario)
+    .delete(controller.removeFuncionario);
+}
